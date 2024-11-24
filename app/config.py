6948ade7c09bc88155708c8810b62965d3bc3ba1
richardesp <espantaleon2001@gmail.com>
2024-11-24 17:53:25 +0000

    feat: create config file for retrieving .env variables

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
