aadaf3fb82415a8061e6c9a5641b8f170f444515
richardesp <espantaleon2001@gmail.com>
2024-11-30 18:24:10 +0000

    chore: delete dotenv library

--- app/config.py
+++ app/config.py
@@ -1,8 +1,4 @@
-from dotenv import load_dotenv
 import os
-
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-load_dotenv(dotenv_path=dotenv_path)
 
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
