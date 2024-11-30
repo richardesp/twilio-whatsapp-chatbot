3e1584ddd8dbce9b94a661f299543b57a8d46353
richardesp <espantaleon2001@gmail.com>
2024-11-30 20:09:00 +0000

    feat: add phone number validator and parameterize phone submitter

--- app/services/whatsapp_service.py
+++ app/services/whatsapp_service.py
@@ -1,47 +1,51 @@
-from twilio.rest import Client
-from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_WHATSAPP_NUMBER, TWILIO_TEMPLATE_CONTENT_SID
-from utils.logger import get_logger
 import json
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-logger = get_logger(__name__)
+from twilio.rest import Client
 
-def send_list_picker(to_number):
+from app.config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_TEMPLATE_CONTENT_SID
+from app.utils.logger import create_logger
+from app.schemas.phone_number import PhoneNumber
+
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+logger = create_logger(__name__)
+
+def send_list_picker(from_number: PhoneNumber, to_number: PhoneNumber):
     try:
         content_sid = TWILIO_TEMPLATE_CONTENT_SID
         
         content_variables = {
-            '1': '',
-            '2': '',
-            '3': '',
-            '4': '',
-            '5': ''
+            '1': 'a',
+            '2': 'b',
+            '3': 'c',
+            '4': 'd',
+            '5': 'e'
         }
 
         message = client.messages.create(
             to=to_number,
-            from_=TWILIO_WHATSAPP_NUMBER,
+            from_=from_number,
             content_sid=content_sid,
             content_variables=json.dumps(content_variables)
         )
 
-        logger.info(f"List picker enviado con SID: {message.sid}")
+        logger.info(f"List picker sent correctly! SID: {message.sid}")
     except Exception as e:
-        logger.error(f"Error al enviar list picker: {e}")
+        logger.error(f"Error trying to send the list picker (check twilio CLI): {e}")
 
-def process_message(from_number, body) -> None:
+def process_message(from_number: PhoneNumber, body: str, to_number: PhoneNumber) -> None:
     body = body.strip().lower()
     logger.info(f'Body retrieved: {body}')
     
     if "hola" in body or "buenas" in body:
         logger.info(f"Mensaje reconocido como saludo. Enviando list picker.")
-        send_list_picker(from_number)  # Enviar el mensaje interactivo con el list picker
+        send_list_picker(from_number=to_number, to_number=from_number)  # Enviar el mensaje interactivo con el list picker
     else:
         # Mensaje genérico si no se reconoce el saludo
         response_message = "Lo siento, no entendí tu mensaje. Puedes decir 'Hola' para ver nuestros servicios."
         try:
             message = client.messages.create(
-                from_=TWILIO_WHATSAPP_NUMBER,
+                from_=to_number,
                 body=response_message,
                 to=from_number
             )
