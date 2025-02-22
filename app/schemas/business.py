73093c1df5ae1009b3d39666a492800dbf2fa545
richardesp <espantaleon2001@gmail.com>
2025-02-22 19:44:07 +0100

    style: replace default=... for ...

--- app/schemas/business.py
+++ app/schemas/business.py
@@ -5,50 +5,48 @@ from app.schemas.phone_number import PhoneNumber
 
 class Service(BaseModel):
     service_name: str = Field(
-        default=...,
+        ...,
         max_length=24,
         description="Name of the service (maximum 24 characters).",
         example="Service 1",
     )
     service_id: str = Field(
-        default=...,
+        ...,
         max_length=200,
         description="Unique identifier for the service (maximum 200 characters).",
         example="service_1_unique_id",
     )
     service_description: str = Field(
-        default=...,
+        ...,
         max_length=72,
         description="Short description of the service (maximum 72 characters).",
         example="Brief description of service 1",
     )
     service_price: str = Field(
-        default=...,
+        ...,
         max_length=15,
         description="Price of the service (maximum 15 characters).",
         example="50€",
     )
     service_duration_minutes: int = Field(
-        default=..., ge=0, description="Duration of the service in minutes.", example=30
+        ..., ge=0, description="Duration of the service in minutes.", example=30
     )
 
 
 class ServiceSelector(BaseModel):
     body: str = Field(
-        default=...,
+        ...,
         max_length=1600,
         description="Welcome message (maximum 1600 characters).",
         example="Welcome to Business Name. We are here to assist you with our services.",
     )
     button_text: str = Field(
-        default=...,
+        ...,
         max_length=20,
         description="Button text to list services (maximum 20 characters).",
         example="List services",
     )
-    services: list[Service] = Field(
-        default=..., description="List of services offered."
-    )
+    services: list[Service] = Field(..., description="List of services offered.")
 
 
 class Business(BaseModel):
--- app/schemas/whatsapp_message.py
+++ app/schemas/whatsapp_message.py
@@ -6,38 +6,24 @@ from pydantic import BaseModel, Field
 
 class WhatsAppMessage(BaseModel):
     SmsMessageSid: str = Field(
-        default=..., description="Unique identifier for the SMS message."
+        ..., description="Unique identifier for the SMS message."
     )
-    NumMedia: int = Field(default=..., description="Number of media attachments.")
+    NumMedia: int = Field(..., description="Number of media attachments.")
     ProfileName: Optional[str] = Field(None, description="Profile name of the sender.")
-    MessageType: str = Field(
-        default=..., description="Type of the message (e.g., text)."
-    )
-    SmsSid: str = Field(default=..., description="Unique identifier for the SMS.")
-    WaId: str = Field(default=..., description="WhatsApp ID of the sender.")
-    SmsStatus: str = Field(
-        default=..., description="Status of the SMS (e.g., received)."
-    )
-    Body: str = Field(default=..., description="Body of the message.")
-    To: str = Field(
-        default=..., description="Recipient's phone number in WhatsApp format."
-    )
-    NumSegments: int = Field(default=..., description="Number of segments in the SMS.")
+    MessageType: str = Field(..., description="Type of the message (e.g., text).")
+    SmsSid: str = Field(..., description="Unique identifier for the SMS.")
+    WaId: str = Field(..., description="WhatsApp ID of the sender.")
+    SmsStatus: str = Field(..., description="Status of the SMS (e.g., received).")
+    Body: str = Field(..., description="Body of the message.")
+    To: str = Field(..., description="Recipient's phone number in WhatsApp format.")
+    NumSegments: int = Field(..., description="Number of segments in the SMS.")
     ReferralNumMedia: int = Field(
-        default=..., description="Number of referral media attachments."
+        ..., description="Number of referral media attachments."
     )
-    MessageSid: str = Field(
-        default=..., description="Unique identifier for the message."
-    )
-    AccountSid: str = Field(
-        default=..., description="Account SID associated with the message."
-    )
-    From: str = Field(
-        default=..., description="Sender's phone number in WhatsApp format."
-    )
-    ApiVersion: str = Field(
-        default=..., description="API version used to send the message."
-    )
+    MessageSid: str = Field(..., description="Unique identifier for the message.")
+    AccountSid: str = Field(..., description="Account SID associated with the message.")
+    From: str = Field(..., description="Sender's phone number in WhatsApp format.")
+    ApiVersion: str = Field(..., description="API version used to send the message.")
 
     @classmethod
     def as_form(
